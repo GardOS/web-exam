91ae8b04629347981a846ff730f485306e5c4465
GardOS <gardo1506@gmail.com>
2018-10-28 16:12:58 +0100

    Fix typo

--- src/server/server.js
+++ src/server/server.js
@@ -56,7 +56,7 @@ app.post("/users", (req, res) => {
         }
 
         const payload = {
-          username: reqUser.username
+          username: newUser.username
         };
 
         const token = jwtSimple.encode(payload, secret);
