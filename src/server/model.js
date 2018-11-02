b11ed9876a68ad7f96bb4dfab65e9ab778018482
GardOS <gardo1506@gmail.com>
2018-11-02 17:15:11 +0100

    Add rule for lowercase on username

--- src/server/model.js
+++ src/server/model.js
@@ -22,7 +22,7 @@ function connectWithRetry() {
 connectWithRetry();
 
 const User = mongoose.model("User", {
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true }
 });
 
