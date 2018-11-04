88969633d126bf601ee66fd074e49fbac175dc2d
GardOS <gardo1506@gmail.com>
2018-11-04 16:16:51 +0100

    Add get for quizzes

--- src/server/quiz-api.js
+++ src/server/quiz-api.js
@@ -3,4 +3,15 @@ const { Quiz } = require("./model");
 
 const router = express.Router();
 
+router.get("/quizzes", (req, res) => {
+  Quiz.find((err, quizzes) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(quizzes);
+    }
+    return null;
+  });
+});
+
 module.exports = router;
