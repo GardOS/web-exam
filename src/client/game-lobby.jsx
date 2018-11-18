ccf3d83de2a8c535b3cd00420d94b45b433cfed0
GardOS <gardo1506@gmail.com>
2018-11-18 16:40:05 +0100

    Add client-side validation of player amount before game start

--- src/client/game-lobby.jsx
+++ src/client/game-lobby.jsx
@@ -4,22 +4,26 @@ import PropTypes from "prop-types";
 const GameLobby = props => (
   <div>
     {props.isHost ? (
-      <button
-        type="button"
-        className="btn btn-block btn-primary align-middle"
-        onClick={() => {
-          props.handleStart();
-        }}
-      >
-        {"Start game"}
-      </button>
+      props.players.length > 1 ? (
+        <button
+          type="button"
+          className="btn btn-block btn-primary align-middle"
+          onClick={() => {
+            props.handleStart();
+          }}
+        >
+          {"Start game"}
+        </button>
+      ) : (
+        <div>Waiting for more players</div>
+      )
     ) : (
       <div>Waiting for host</div>
     )}
 
     <div>Players:</div>
     {props.players.map(player => (
-      <div>{player}</div>
+      <div key={player}>{player}</div>
     ))}
   </div>
 );
